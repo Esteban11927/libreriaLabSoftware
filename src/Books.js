df2ddf638f990a0e53f0404bdcd9d45a51ca8e23
Esteban11927 <carlosesteban.ospina@utp.edu.co>
2022-04-28 23:02:03 -0500

    busqueda funcionando sin recargar v1

--- src/Books.js
+++ src/Books.js
@@ -5,22 +5,41 @@ import testPic from './newsTestPic.jpg';
 let img = require('./newsTestPic.jpg');
 function Books(){
     const [books, setBooks] = useState([]);
-    var query = "dragon ball z";
-    var url = "https://www.googleapis.com/books/v1/volumes?q="+query.replace(' ', '+');
     var opt = {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
         'Content-Type': 'application/json',
         mode: 'cors',
     };
     const req = () => {
+        let query = document.getElementById("searchBookInput").value;
+        if(query == "") query = "dragon ball z";
+        let url = "https://www.googleapis.com/books/v1/volumes?q="+query.replace(' ', '+');
         fetch(url, opt)
         .then((response) => {return response.json()})
         .then((json) => {setBooks(json.items);})
         .catch(()=>{console.log("errorsito");});
     }
     useEffect(req, []);
+    const handleKeyDown = (e) => {
+        req();
+        e.preventDefault();
+    }
+    //document.getElementsByClassName("search")[0].submit(() => {return false});
     return (
         <div className="books">
+            <form action="#" method='post' className="search" onSubmit={handleKeyDown}>
+                <div className="searchBook">
+                    <label htmlFor="searchBook">Buscar libro</label><br />
+                    <input type="text" name="book" id="searchBookInput"/>
+                </div>
+                <div className="searchFilter">
+                    <label htmlFor="searchFilter">Filtrar búsqueda</label><br />
+                    <select name="filter" id="searchFilterList">
+                        <option value="todo">Todo</option>
+                        <option value="disponibles">Disponibles</option>
+                    </select>
+                </div>
+            </form>
             <h1>Libros</h1>
             <ul className="booksQuery">
                 {books.map((book, index) => {
